75fed0c898759e3a9ecb90feb48132addcf57748
Amadiane <129948408+Amadiane@users.noreply.github.com>
2026-03-30 21:06:13 +0100

    hi

--- Server/Base/models.py
+++ Server/Base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    
+    ROLE_CHOICES = (
+        ("admin", "Admin"),
+        ("vendeur", "Vendeur"),
+        ("manager", "Manager"),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
